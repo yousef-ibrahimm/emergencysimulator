d23611390ac18a0a860fb36ec65a74cca6c45a20
YosefYasser <yousefibrahim2614@gmail.com>
2022-12-13 00:16:18 +0000

    created first part of demo
    Created the first concept of the demo where a node is created and some event listeners

---
+++ demo/script.js
@@ -0,0 +1,24 @@
+"use strict";
+const textBox = document.getElementById("textbox");
+const btn1 = document.getElementById("btn1");
+
+//array of nodes where you push each new node until story is done
+const storyArray = [
+  {
+    id: 0,
+    text: "try",
+    buttons: {
+      btn1: {
+        text: "move forward",
+      },
+      btn2: {
+        text: "button 2 has been pressed",
+      },
+    },
+  },
+];
+
+btn1.addEventListener("click", () => {
+  textBox.innerText = storyArray[0].text;
+  btn1.textContent = storyArray[0].buttons.btn1.text;
+});
