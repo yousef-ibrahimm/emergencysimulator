18eb9f600945b8f146f4e2dec71a49f69d68a18f
YosefYasser <121143291+yousef-ibrahimm@users.noreply.github.com>
2023-01-27 12:34:43 +0000

    all buttons working

--- demo_json/script.js
+++ demo_json/script.js
@@ -1,6 +1,9 @@
 "use strict";
 const textBox = document.getElementById("textbox");
-const btn1 = document.querySelectorAll("btn1");
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
 
 let story,
   author,
@@ -40,4 +43,24 @@ btn1.addEventListener("click", (e) => {
   nextQuestion(story[start]);
 });
 
+btn2.addEventListener("click", (e) => {
+  const button = e.target.id;
+  console.log(e.target.id);
+  start = story[start].buttons[button].id;
+  nextQuestion(story[start]);
+});
+
+btn3.addEventListener("click", (e) => {
+  const button = e.target.id;
+  console.log(e.target.id);
+  start = story[start].buttons[button].id;
+  nextQuestion(story[start]);
+});
+
+btn4.addEventListener("click", (e) => {
+  const button = e.target.id;
+  console.log(e.target.id);
+  start = story[start].buttons[button].id;
+  nextQuestion(story[start]);
+});
 console.log(story);
