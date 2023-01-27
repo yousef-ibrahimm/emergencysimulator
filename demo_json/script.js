8a9a8d4a40e8cf99a4466601b56121b11664c35b
YosefYasser <121143291+yousef-ibrahimm@users.noreply.github.com>
2023-01-27 12:25:43 +0000

    story works now

--- demo_json/script.js
+++ demo_json/script.js
@@ -1,11 +1,11 @@
 "use strict";
 const textBox = document.getElementById("textbox");
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
+const btn1 = document.querySelectorAll("btn1");
 
-let story, author, date;
+let story,
+  author,
+  date,
+  start = 0;
 
 function addData(data) {
   story = data.data;
@@ -16,9 +16,8 @@ async function loadData() {
   const response = await fetch("data.json");
   if (response.ok) {
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     addData(data);
-    // storyArray.push(data);
   } else {
     console.log("no data received");
   }
@@ -26,14 +25,19 @@ async function loadData() {
 
 function nextQuestion(obj) {
   textBox.textContent = obj.text;
-  btn1.innerHTML = obj.buttons.btn1.text;
-  btn2.innerHTML = obj.buttons.btn2.text;
-  btn3.innerHTML = obj.buttons.btn3.text;
-  btn4.innerHTML = obj.buttons.btn4.text;
+  btn1.textContent = obj.buttons.btn1.text;
+  btn2.textContent = obj.buttons.btn2.text;
+  btn3.textContent = obj.buttons.btn3.text;
+  btn4.textContent = obj.buttons.btn4.text;
 }
 
 window.addEventListener("load", loadData);
 
-btn1.addEventListener("click", () => {
-  nextQuestion(story[0]);
+btn1.addEventListener("click", (e) => {
+  const button = e.target.id;
+  console.log(e.target.id);
+  start = story[start].buttons[button].id;
+  nextQuestion(story[start]);
 });
+
+console.log(story);
